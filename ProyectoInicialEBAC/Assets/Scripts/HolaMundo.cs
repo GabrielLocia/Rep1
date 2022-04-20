cb7b4002ee076ebcaf74df05c7019ebac65d551b
Gabriel Locia <locciaa1@gmail.com>
2022-04-19 21:07:33 -0500

    Agregando funciones del game loop

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -6,14 +6,23 @@ public class HolaMundo : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public static GameObject  myobject;
+    private int count;
+    private void Awake() {
+        myobject = this.gameObject;
+        count = 0;
+        Debug.Log("Inicializando mi objeto: " + myobject.name);
+    }
+
     void Start()
     {
-
+        Debug.LogWarning("El valor inicial de count es igual a = " + count);
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+
+      Debug.LogError("El valor de count incrementa: " + count++ );
     }
 }
